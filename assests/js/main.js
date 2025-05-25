431a6092bcb2af26557e7ebd03a66e77a1b16cbf
unknown <judeanzelmo@gmail.com>
2025-05-25 20:41:55 +0800

    new

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,33 +1,33 @@
-const navlink = document.querySelectorAll('.nav__link');
+const navlink = document.querySelectorAll(".nav__link");
 
 function activeLink() {
-    navlink.forEach((a) => a.classList.remove('active-link'));
-    this.classList.add('active-link');
-    }
+  navlink.forEach((a) => a.classList.remove("active-link"));
+  this.classList.add("active-link");
+}
 
-navlink.forEach((a) => a.addEventListener('click', activeLink));
+navlink.forEach((a) => a.addEventListener("click", activeLink));
 
 function scrollHeader() {
-    const header = document.getElementById('header');
-    if (this.scrollY >= 50) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
-var mixerProjects = mixitup('.projects__container', {
-    selectors: {
-        target: '.project__item'
-    },
-    animation: {
-        duration: 300
-    }
+var mixerProjects = mixitup(".projects__container", {
+  selectors: {
+    target: ".project__item",
+  },
+  animation: {
+    duration: 300,
+  },
 });
 
-const linkWork = document.querySelectorAll('.category__btn');
+const linkWork = document.querySelectorAll(".category__btn");
 
 function activeWork() {
-    linkWork.forEach((a) => a.classList.remove('active-work'));
-    this.classList.add('active-work');
-    }
+  linkWork.forEach((a) => a.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
 
-linkWork.forEach((a) => a.addEventListener('click', activeWork));
+linkWork.forEach((a) => a.addEventListener("click", activeWork));
