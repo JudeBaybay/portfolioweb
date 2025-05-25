e73620b0b59683a0468a18e5e8d460a1637c16ba
unknown <judeanzelmo@gmail.com>
2025-05-25 18:08:00 +0800

    new

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,8 +1,33 @@
-const navlinks = document.querySelectorAll('.nav__btn');
+const navlink = document.querySelectorAll('.nav__link');
 
 function activeLink() {
-    navlinks.forEach((a) => a.classList.remove('active-link'));
+    navlink.forEach((a) => a.classList.remove('active-link'));
     this.classList.add('active-link');
-}
+    }
 
-navlinks.forEach((a) => a.addEventListener('click', activeLink));
+navlink.forEach((a) => a.addEventListener('click', activeLink));
+
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+var mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork() {
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+    }
+
+linkWork.forEach((a) => a.addEventListener('click', activeWork));
