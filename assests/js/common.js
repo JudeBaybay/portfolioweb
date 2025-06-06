694503946e05908729645540313e992981debe55
unknown <judeanzelmo@gmail.com>
2025-06-06 14:58:43 +0800

    new

--- assests/js/common.js
+++ assests/js/common.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
       awardsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 
-    toggleBtn.textContent = isExpanded ? 'Show More' : 'Show Less';
+    toggleBtn.textContent = isExpanded ? 'Show More 🠧' : 'Show Less 🠥';
     isExpanded = !isExpanded;
   });
 });
